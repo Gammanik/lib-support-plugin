3d12b17d6126fe7bc3817bd9b7ca2e249880f180
Bobyshev Nikita <Gammanik@gmail.com>
2020-03-16 17:37:49 +0300

    add ShowPsiAction stub

--- src/main/kotlin/ShowPsiAction.kt
+++ src/main/kotlin/ShowPsiAction.kt
@@ -1,32 +1,38 @@
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.LangDataKeys
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.pom.Navigatable
+import com.intellij.psi.PsiFile
 
-
-class PopupDialogAction : AnAction() {
-
-    // Using the event, evaluate the context, and enable or disable the action.
-    override fun update(e: AnActionEvent) {
-        // Set the availability based on whether a project is open
-        // Set the availability based on whether a project is open
-        val project = e.project
-        e.presentation.isEnabledAndVisible = project != null
-    }
-
-    // Using the event, implement an action. For example, create and show a dialog.
+class ShowPsiAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        // Using the event, create and show a dialog
         val currentProject: Project = e.project!!
+        // todo: PsiManager.getInstance(currentProject).addPsiTreeChangeListener()
+
         val dlgMsg = StringBuffer(e.presentation.text.toString() + " Selected!")
-        val dlgTitle: String = e.presentation.description
+        val dlgTitle = "description" // e.presentation.description
+
         // If an element is selected in the editor, add info about it.
         val nav: Navigatable? = e.getData(CommonDataKeys.NAVIGATABLE)!!
         if (nav != null) {
             dlgMsg.append(java.lang.String.format("\nSelected Element: %s", nav.toString()))
         }
+        dlgMsg.append("\n\n")
+        val psiFile : PsiFile? = e.getData(LangDataKeys.PSI_FILE)
+        if (psiFile != null) {
+            dlgMsg.append(psiFile.toString())
+        }
+        dlgMsg.append(psiFile)
         Messages.showMessageDialog(currentProject, dlgMsg.toString(), dlgTitle, Messages.getInformationIcon())
     }
+
+    override fun update(e: AnActionEvent) {
+        val project = e.project
+        e.presentation.isEnabledAndVisible = project != null
+    }
+
 }
+
