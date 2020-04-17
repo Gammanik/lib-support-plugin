b6e45dbc49b8f6b8565a44e1184b4e741fa09406
Bobyshev Nikita <Gammanik@gmail.com>
2020-04-17 16:22:36 +0300

    finally made the script with dependencies work

--- src/main/kotlin/ShowPsiAction.kt
+++ src/main/kotlin/ShowPsiAction.kt
@@ -1,33 +1,36 @@
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.components.service
-import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.module.ModuleManager
-import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
-import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.ui.Messages
-import org.apache.log4j.Level
+import org.jetbrains.kotlin.cli.common.repl.ScriptArgsWithTypes
 import org.jetbrains.kotlin.jsr223.KotlinJsr223JvmScriptEngine4Idea
 import org.jetbrains.kotlin.jsr223.KotlinJsr223StandardScriptEngineFactory4Idea
+import org.jetbrains.kotlin.script.util.scriptCompilationClasspathFromContextOrStlib
 import java.io.File
 import java.io.FileReader
-import java.io.Reader
-import java.util.jar.Attributes
-import java.util.jar.JarFile
-import javax.script.Invocable
+import javax.script.ScriptContext
 import javax.script.ScriptEngine
-import kotlin.script.experimental.api.EvaluationResult
-import kotlin.script.experimental.api.ResultWithDiagnostics
-import kotlin.script.experimental.host.toScriptSource
+import kotlin.script.experimental.jvm.util.KotlinJars
 
 class ShowPsiAction : AnAction() {
-    override fun actionPerformed(e: AnActionEvent) {
-        //todo: use  val s = p.service<MethodRegService>()
+    override fun actionPerformed(e: AnActionEvent) { //todo: use  val s = p.service<MethodRegService>()
+        val jarNames: List<File> = listOf(File("/Users/Nikita/Documents/lib-support/dsl-lib-support/build/libs/dsl-lib-support.jar")) + scriptCompilationClasspathFromContextOrStlib(wholeClasspath = true) + KotlinJars.kotlinScriptStandardJars
+        val factory = KotlinJsr223StandardScriptEngineFactory4Idea()
+        val engine: ScriptEngine = KotlinJsr223JvmScriptEngine4Idea(
+            factory,
+            jarNames,
+            "kotlin.script.templates.standard.ScriptTemplateWithBindings",
+            { ctx, argTypes -> ScriptArgsWithTypes(arrayOf(ctx.getBindings(ScriptContext.ENGINE_SCOPE)), argTypes ?: emptyArray()) },
+            arrayOf(Map::class)
+        )
+        val script = FileReader(File(e.project!!.basePath + "/src/main/resources/support/example.kts"))
+        val res = engine.eval(script)
+
         val s = ProjectManager.getInstance().defaultProject.service<MethodRegService>()
         s.updateMarkedMethods(mapOf(Pair("LibClassOne", "sMethodOne")
                 to MethodToMark("LibClassOne", "sMethodOne")))
-        Messages.showMessageDialog("sup", "title", Messages.getInformationIcon())
+        Messages.showMessageDialog(res.toString(), "title", Messages.getInformationIcon())
     }
 
     override fun update(e: AnActionEvent) {
