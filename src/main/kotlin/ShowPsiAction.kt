842e268206ca82e2daa38d2b54ae8b5f43794b56
Bobyshev Nikita <Gammanik@gmail.com>
2020-04-30 00:45:11 +0300

    1: finally loading dsl jar with no ClassCastException

--- src/main/kotlin/ShowPsiAction.kt
+++ src/main/kotlin/ShowPsiAction.kt
@@ -1,37 +1,38 @@
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.PathManager.getPluginsPath
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.util.io.FileUtil.toSystemIndependentName
 import org.jetbrains.kotlin.cli.common.repl.ScriptArgsWithTypes
 import org.jetbrains.kotlin.jsr223.KotlinJsr223JvmScriptEngine4Idea
 import org.jetbrains.kotlin.jsr223.KotlinJsr223StandardScriptEngineFactory4Idea
-import org.jetbrains.kotlin.script.util.scriptCompilationClasspathFromContextOrStlib
 import java.io.File
 import java.io.FileReader
 import javax.script.ScriptContext
-import javax.script.ScriptEngine
 import kotlin.script.experimental.jvm.util.KotlinJars
 
 class ShowPsiAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) { //todo: use  val s = p.service<MethodRegService>()
-        val jarNames: List<File> = listOf(File("/Users/Nikita/Documents/lib-support/dsl-lib-support/build/libs/dsl-lib-support.jar")) + scriptCompilationClasspathFromContextOrStlib(wholeClasspath = true) + KotlinJars.kotlinScriptStandardJars
+        Thread.currentThread().contextClassLoader = this.javaClass.classLoader
+        val dslPath = toSystemIndependentName(getPluginsPath() + "/lib-support/lib/dsl-lib-support.jar")
+        val jarNames: List<File> = KotlinJars.kotlinScriptStandardJars + listOf(File(dslPath))
+
         val factory = KotlinJsr223StandardScriptEngineFactory4Idea()
-        val engine: ScriptEngine = KotlinJsr223JvmScriptEngine4Idea(
+        val engine = KotlinJsr223JvmScriptEngine4Idea(
             factory,
             jarNames,
             "kotlin.script.templates.standard.ScriptTemplateWithBindings",
             { ctx, argTypes -> ScriptArgsWithTypes(arrayOf(ctx.getBindings(ScriptContext.ENGINE_SCOPE)), argTypes ?: emptyArray()) },
             arrayOf(Map::class)
         )
-        val script = FileReader(File(e.project!!.basePath + "/src/main/resources/support/example.kts"))
-//        val res = engine.eval(script)
+        val scriptFile = FileReader(File(e.project!!.basePath + "/src/main/resources/support/example.kts"))
+        val res: Map<String, MethodToMark>? = engine.eval(scriptFile) as Map<String, MethodToMark>?
 
-        val s = ProjectManager.getInstance().defaultProject.service<MethodRegService>()
-        s.updateMarkedMethods(mapOf("lib.LibClassOne.method1"
-                to MethodToMark("lib.LibClassOne.method1", "sMethodOne")
-        ))
-        Messages.showMessageDialog("res.toString()", "title", Messages.getInformationIcon())
+        val service = ProjectManager.getInstance().defaultProject.service<MethodRegService>()
+        service.updateMarkedMethods(res!!)
+        Messages.showMessageDialog("$res", "title", Messages.getInformationIcon())
     }
 
     override fun update(e: AnActionEvent) {
