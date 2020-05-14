0b50200d5c3b32f286ed809cdc94ba78610c74d4
Bobyshev Nikita <Gammanik@gmail.com>
2020-05-15 01:24:00 +0300

    add parse jars to find the config

--- src/main/kotlin/ShowPsiAction.kt
+++ src/main/kotlin/ShowPsiAction.kt
@@ -3,18 +3,42 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.PathManager.getPluginsPath
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.roots.LibraryOrderEntry
+import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.roots.OrderEntry
+import com.intellij.openapi.roots.OrderRootType
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.util.io.FileUtil.toSystemIndependentName
+import com.intellij.openapi.vfs.JarFileSystem
 import org.jetbrains.kotlin.cli.common.repl.ScriptArgsWithTypes
+import org.jetbrains.kotlin.idea.util.projectStructure.allModules
 import org.jetbrains.kotlin.jsr223.KotlinJsr223JvmScriptEngine4Idea
 import org.jetbrains.kotlin.jsr223.KotlinJsr223StandardScriptEngineFactory4Idea
 import java.io.File
-import java.io.FileReader
+import java.io.InputStreamReader
+import java.io.Reader
 import javax.script.ScriptContext
 import kotlin.script.experimental.jvm.util.KotlinJars
 
 class ShowPsiAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) { //todo: use  val s = p.service<MethodRegService>()
+        val module = ModuleRootManager.getInstance(e.project?.allModules()!![2])
+        val orderEntries: Array<OrderEntry> = module.orderEntries
+
+        orderEntries.filterIsInstance<LibraryOrderEntry>()
+            .forEach {
+                val libJars = it.library!!.getFiles(OrderRootType.CLASSES)
+                val libRoot = JarFileSystem.getInstance().findFileByPath(libJars[0].path)
+                val script = libRoot?.findChild("META-INF")
+                    ?.findChild("lib-support")?.findChild("settings.kts")
+
+                if (script != null) {
+                    updateMarkedMethods(InputStreamReader(script.inputStream))
+                }
+            }
+    }
+
+    private fun updateMarkedMethods(scriptFile: Reader) {
         val dslPath = toSystemIndependentName(getPluginsPath() + "/lib-support/lib/dsl-lib-support.jar")
         val jarNames: List<File> = KotlinJars.kotlinScriptStandardJars + listOf(File(dslPath))
 
@@ -26,7 +50,6 @@ class ShowPsiAction : AnAction() {
             { ctx, argTypes -> ScriptArgsWithTypes(arrayOf(ctx.getBindings(ScriptContext.ENGINE_SCOPE)), argTypes ?: emptyArray()) },
             arrayOf(Map::class)
         )
-        val scriptFile = FileReader(File(e.project!!.basePath + "/src/main/resources/support/example.kts"))
         val res: Map<String, MethodToMark> = withCorrectClassLoader { engine.eval(scriptFile) as Map<String, MethodToMark> }
 
         val service = ProjectManager.getInstance().defaultProject.service<MethodRegService>()
@@ -34,7 +57,7 @@ class ShowPsiAction : AnAction() {
         Messages.showMessageDialog("$res", "title", Messages.getInformationIcon())
     }
 
-    private fun <T>withCorrectClassLoader(action: () -> T) : T {
+    private fun <T>withCorrectClassLoader(action: () -> T?) : T {
         var res: T? = null
         val oldClassLoader = Thread.currentThread().contextClassLoader
         Thread.currentThread().contextClassLoader = this.javaClass.classLoader
